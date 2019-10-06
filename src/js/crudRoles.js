00781910c00285666af3e8ee07b23c143d6eb79f
Antonio de Jesus Hernandez Liborio <47048762+AntonioDeJesusHernandezLiborio@users.noreply.github.com>
2019-10-06 12:37:07 -0500

    MEJORANDO LA VALIDACION Y ALERT
    CHECALO HOMMIE

--- src/js/crudRoles.js
+++ src/js/crudRoles.js
@@ -1,89 +1,105 @@
-const rol= new Vue({
-	el:'#tablaRoles',
-	data:{
-		columna:'Codigo',
-		columna2: 'Nombre',
-        columna3:'Acciones',
-        mensajesA:null,
+const rol = new Vue({
+    el: '#tablaRoles',
+    data: {
+        mensajesA: null,
         tipoalertaA: null,
-        mensajesE:null,
+        mensajesE: null,
         tipoalertaE: null,
-        roles:[],
-        elegido:{}
+        mensajesEli: null,
+        tipoalertaEli: null,
+        roles: [],
+        elegido: {}
     },
-    mounted:function(){
+    mounted: function () {
         this.cargaTabla()
     },
-    methods:{
-        cargaTabla:function(){
-            let formdata =  new FormData();
-            formdata.append("option","showdata")
-            axios.post("http://localhost/7moProyecto/pry-quickimage/controller/rol_controller.php",formdata)
-            .then(function(response){
-                console.log(response);
-                rol.roles=response.data.roles;
-            })
-        },
-        nuevoRol:function(){
-            if((document.getElementById("nombreRol").value) == ""){
-                rol.tipoalertaA='alert alert-danger',
-                rol.mensajesA = 'Campos vacios'
-                return false;
-            }else{
-                let formdata =  new FormData();
-                formdata.append("option","insert")
-                formdata.append("nombre",document.getElementById("nombreRol").value)
-                axios.post("http://localhost/7moProyecto/pry-quickimage/controller/rol_controller.php",formdata)
-                .then(function(response){
-                    if(response.data == 1){
-                        rol.cargaTabla()
-                        document.getElementById("nombreRol").value='',
-                        rol.tipoalertaA='alert alert-success',
-                        rol.mensajesA = 'Rol agregado correctamente'
-                    }else if(response.data == ""){
-                        rol.tipoalertaA='alert alert-danger',
-                        rol.mensajesA = 'Rol ya existe'
-                    }
-                    
+    methods: {
+        cargaTabla: function () {
+            let formdata = new FormData();
+            formdata.append("option", "showdata")
+            axios.post("http://localhost/7moProyecto/pry-quickimage/controller/rol_controller.php", formdata)
+                .then(function (response) {
+                    console.log(response);
+                    rol.roles = response.data.roles;
                 })
+        },
+        nuevoRol: function () {
+            if ((document.getElementById("nombreRol").value) == "") {
+                rol.tipoalertaA = 'alert alert-danger',
+                    rol.mensajesA = 'Campos vacios'
+            } else {
+                let formdata = new FormData();
+                formdata.append("option", "insert")
+                formdata.append("nombre", document.getElementById("nombreRol").value)
+                axios.post("http://localhost/7moProyecto/pry-quickimage/controller/rol_controller.php", formdata)
+                    .then(function (response) {
+                        if (response.data == 1) {
+                            rol.cargaTabla()
+                            document.getElementById("nombreRol").value = '',
+                                rol.tipoalertaA = 'alert alert-success',
+                                rol.mensajesA = 'Rol agregado correctamente'
+                        } else if (response.data == "") {
+                            rol.tipoalertaA = 'alert alert-danger',
+                                rol.mensajesA = 'Error, el rol ya existe'
+                        }
+
+                    })
             }
         },
-        editarRol:function(){
-            if((document.getElementById("nombreEditar_rol").value) == ""){
-                rol.tipoalertaE='alert alert-danger',
-                rol.mensajesE = 'Campo vacios'
+        editarRol: function () {
+            if ((document.getElementById("nombreEditar_rol").value) == "") {
+                rol.tipoalertaE = 'alert alert-danger',
+                    rol.mensajesE = 'Campo vacios'
                 return false;
-            }else{
-                let formdata =  new FormData();
-                formdata.append("option","update")
-                formdata.append("id",document.getElementById("codigoEditar_rol").value)
-                formdata.append("nombre",document.getElementById("nombreEditar_rol").value)
-                axios.post("http://localhost/7moProyecto/pry-quickimage/controller/rol_controller.php",formdata)
-                .then(function(response){
-                    if(response.data == 1){
-                        rol.cargaTabla()
-                        rol.tipoalertaE='alert alert-success',
-                        rol.mensajesE = 'El rol se ah editado corrrectamente'
-                    }
-                })
+            } else {
+                let formdata = new FormData();
+                formdata.append("option", "update")
+                formdata.append("id", document.getElementById("codigoEditar_rol").value)
+                formdata.append("nombre", document.getElementById("nombreEditar_rol").value)
+                axios.post("http://localhost/7moProyecto/pry-quickimage/controller/rol_controller.php", formdata)
+                    .then(function (response) {
+                        if (response.data == 1) {
+                            rol.cargaTabla()
+                            rol.tipoalertaE = 'alert alert-success',
+                            rol.mensajesE = 'El rol se ah editado corrrectamente'
+                        }
+                    })
             }
         },
-        eliminarRol:function(){
-            let formdata =  new FormData();
-            formdata.append("option","delete")
-            formdata.append("id",document.getElementById("codigoEliminar_rol").value)
-            axios.post("http://localhost/7moProyecto/pry-quickimage/controller/rol_controller.php",formdata)
-            .then(function(response){
-                if(response.data == 1){
-                    rol.cargaTabla()
-                }
-            })
+        eliminarRol: function () {
+            if ((document.getElementById("nombreEditar_rol").value) == "") {
+                rol.tipoalertaEli = 'alert alert-danger',
+                rol.mensajesEli = 'Campo vacio'
+            } else {
+                let formdata = new FormData();
+                formdata.append("option", "delete")
+                formdata.append("id", document.getElementById("codigoEliminar_rol").value)
+                axios.post("http://localhost/7moProyecto/pry-quickimage/controller/rol_controller.php", formdata)
+                    .then(function (response) {
+                        if (response.data == 1) {
+                            rol.cargaTabla()
+                            rol.tipoalertaEli = 'alert alert-success',
+                            rol.mensajesEli = 'El rol se ah eliminado corrrectamente'
+                        }else if(response.data == ""){
+                            rol.tipoalertaEli = 'alert alert-danger',
+                            rol.mensajesEli = 'Error al eliminar, el rol esta asignado a por lo menos 1 usuario'
+                        }
+                    })
+            }
         },
-        pasarDatosEditar(Rol){
-            rol.elegido=Rol;
+        pasarDatosEditar(Rol) {
+            rol.elegido = Rol;
         },
-        pasarDatosEliminar(Rol){
-            rol.elegido=Rol;
+        pasarDatosEliminar(Rol) {
+            rol.elegido = Rol;
+        },
+        limpiar(){
+            rol.mensajesA= null,
+            rol.tipoalertaA= null,
+            rol.mensajesE= null,
+            rol.tipoalertaE= null,
+            rol.mensajesEli= null,
+            rol.tipoalertaEli=null
         }
     }
 });

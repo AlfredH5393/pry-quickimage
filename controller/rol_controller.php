02695f14cd8a96131af533db3ef3bfcb6804815f
Antonio de Jesus Hernandez Liborio <47048762+AntonioDeJesusHernandezLiborio@users.noreply.github.com>
2019-10-06 06:02:01 -0500

    CRUD ROLES
    CRUP ROLES COMPLETO CON VUE.JS, VALIDADOS Y CON PROCEDIMIENTOS ALMACENADOS, AGREGUE LA CARPETA DONDE ESTA LA BASE DE DATOS

---
+++ controller/rol_controller.php
@@ -0,0 +1,34 @@
+<?php
+    require('../model/rol.php');
+
+    $operacion=$_POST['option'];
+    
+    $rol= new rol();
+
+    switch($operacion)
+    {
+        case 'insert':
+            echo $rol->insert($_POST['nombre']);
+        break;
+
+        case 'update':
+        echo $rol->update($_POST['id'],$_POST['nombre']);
+        break;
+
+        case 'delete':
+        echo $rol->delete($_POST['id']);
+        break;
+
+        case 'showdata':
+            echo $rol->getdatos();
+        break;
+
+      
+    break;
+        
+
+    }
+
+
+
+?>

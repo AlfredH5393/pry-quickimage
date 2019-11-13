64b5cffb16c34eed952e25496848d533a2ee89db
alfredo <alfredo@turismo.com>
2019-11-12 20:37:17 -0600

    Se agrego las imagenes faltantes

--- model/conexion.php
+++ model/conexion.php
@@ -3,7 +3,7 @@
 class conexion
 {
     const user='root';
-    const pass='';
+    const pass='admin1';
     const db='dbquickimage';
     const servidor='localhost';
     // const user='toolcrea_turismo';
